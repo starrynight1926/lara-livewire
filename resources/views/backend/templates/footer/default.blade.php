8ea0c083246bb7e4912af5fc2697c28037537ef0
starrynight <starrynight1926@gmail.com>
2025-01-27 23:46:26 +0700

    add html layout

---
+++ resources/views/backend/templates/footer/default.blade.php
@@ -0,0 +1,14 @@
+<!--! BEGIN: Vendors JS !-->
+<script src="{{asset('html/assets/vendors/js/vendors.min.js')}}"></script>
+<!-- vendors.min.js {always must need to be top} -->
+<script src="{{asset('html/assets/vendors/js/daterangepicker.min.js')}}"></script>
+<script src="{{asset('html/assets/vendors/js/apexcharts.min.js')}}"></script>
+<script src="{{asset('html/assets/vendors/js/circle-progress.min.js')}}"></script>
+<!--! END: Vendors JS !-->
+<!--! BEGIN: Apps Init  !-->
+<script src="{{asset('html/assets/js/common-init.min.js')}}"></script>
+<script src="{{asset('html/assets/js/dashboard-init.min.js')}}"></script>
+<!--! END: Apps Init !-->
+<!--! BEGIN: Theme Customizer  !-->
+<script src="{{asset('html/assets/js/theme-customizer-init.min.js')}}"></script>
+<!--! END: Theme Customizer !-->
